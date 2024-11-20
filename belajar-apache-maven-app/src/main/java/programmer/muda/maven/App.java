3bce3d329e9aea123d08d68f0a6239c4ada571a3
SAR09 <rizkymuu5@gmail.com>
2024-11-20 13:33:53 +0700

    First commit

---
+++ belajar-apache-maven-app/src/main/java/programmer/muda/maven/App.java
@@ -0,0 +1,16 @@
+package programmer.muda.maven;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+
+        Person person = new Person("Saiful Amin Rizki");
+        String json = gson.toJson(person);
+        System.out.println(json);
+    }
+}
